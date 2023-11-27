8f8013bdd5890be5a5935a94d63c34c75da18e74
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2023-11-27 14:32:13 -0300

    fixed error on alert alias

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -1,6 +1,6 @@
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
-which notify-send &> /dev/null/ && alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+which notify-send &> /dev/null && alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 alias ll='ls -alF'
 alias la='ls -A'
