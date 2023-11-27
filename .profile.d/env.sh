bbfde7ab600c9f08cad150c15714cf11c600e76d
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2023-11-27 13:58:06 -0300

    added umask comment on profile conf

--- .profile.d/env.sh
+++ .profile.d/env.sh
@@ -1,2 +1,6 @@
+# the default umask is set in /etc/profile; for setting the umask
+# for ssh logins, install and configure the libpam-umask package.
+#umask 022
+
 export PATH=$PATH:~/.local/bin
 export EDITOR=nvim
