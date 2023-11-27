2fdbe3dd3c1620c336bb6b3ce5bb467215b22318
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2023-11-27 13:18:42 -0300

    splitting .bashrc into different files

--- .bash_aliases
+++
@@ -1,4 +0,0 @@
-alias fp="fzf-into-tmux"
-alias ff="fzf-into-edit"
-alias python="python3"
-alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
---
+++ .bashrc.d/aliases.bash
@@ -0,0 +1,13 @@
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+which notify-send &> /dev/null/ && alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias fp="fzf-into-tmux"
+alias ff="fzf-into-edit"
+alias python="python3"
+alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
