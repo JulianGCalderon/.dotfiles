5ffbc20587ccfbdbff1de19e313f42c7a21a6dd6
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-01-21 16:16:37 -0300

    update nvim conf

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,14 @@ return {
   'stevearc/oil.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   lazy = false,
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true,
+      is_always_hidden = function(name, bufnr)
+        return name == '.git'
+      end,
+    },
+  },
   keys = {
     { '-', ':Oil<CR>', desc = 'Oil [E]xplorer' },
   },
