28a0463f5d53e4de052bfb56e9e233ca86356293
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-04-10 23:39:47 -0300

    Remove unused

--- bash/.bashrc.d/aliases.bash
+++
@@ -1,7 +0,0 @@
-# Add an "alert" alias for long running commands.  Use like so:
-#   sleep 10; alert
-which notify-send &> /dev/null && alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
-
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
