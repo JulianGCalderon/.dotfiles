ba618c87818a3cc3fad0ead51c0559645503e0d0
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-04-29 00:47:07 -0300

    Add custom prompt

--- zsh/dot-zshrc.d/00.zsh
+++ zsh/dot-zshrc.d/00.zsh
@@ -1,1 +1,21 @@
+# PROMPT
+
+autoload -U colors && colors
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' formats "%b"
+
+precmd () { 
+  vcs_info
+
+  PROMPT="%{$fg[green]%}%~ "
+  [[ -n $vcs_info_msg_0_ ]] && {
+      [[ -n $(git status --porcelain) ]] && {
+        GIT_STATUS=" %{$fg[red]%}✗%{$fg[blue]%}"
+      }
+      PROMPT+="%{$fg[blue]%}($vcs_info_msg_0_$GIT_STATUS) "
+  }
+  PROMPT+="%{$reset_color%}"
+}
+
 export COLORTERM=truecolor
