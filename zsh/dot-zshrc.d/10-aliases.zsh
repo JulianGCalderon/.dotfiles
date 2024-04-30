617808ae22d693fd2f6f76c44d4c2f65a3dd7c49
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-04-30 10:06:40 -0300

    Move ls to the top

--- zsh/dot-zshrc.d/10-aliases.zsh
+++ zsh/dot-zshrc.d/10-aliases.zsh
@@ -1,7 +1,8 @@
+alias ls="ls --color=auto"
+
 alias e=$EDITOR
 alias n="nnn -e"
 alias lg="lazygit"
 alias j="journal"
 alias zk="zoxide-kitty"
-alias ls="ls --color=auto"
 alias dot="(cd ~/.dotfiles && hx .)"
