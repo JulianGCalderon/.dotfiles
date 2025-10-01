8f98295c32e8c7a292fdb40966c9dc329dbe822b
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-10-01 12:40:00 -0300

    Remove pyenv, add ASDF

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -5,4 +5,14 @@ end
 zoxide init fish | source
 starship init fish | source
 direnv hook fish | source
-pyenv init - | source
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
