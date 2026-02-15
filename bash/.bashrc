ecbc10cf9704ef419e3c24db85f4abe269508f58
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2026-02-15 00:21:43 +0000

    Migrate to Linux (WSL) with Alacritty and Zellij

--- bash/.bashrc
+++
@@ -1,1 +0,0 @@
-source ~/.config/fish/fish.bash
---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,9 @@
+set -x EDITOR hx
+set -x PATH /usr/local/go/bin $PATH
+set -x PATH "$HOME/.cargo/bin" $PATH
+set -x PATH "$HOME/.go/bin" $PATH
+set -x PATH "$HOME/.opencode/bin" $PATH
+
+if type -q starship
+    starship init fish | source
+end
